be37e40ba92e900666fd7e10ba9152236cfdae21
plemanach <plemanach@hotmail.com>
2016-12-27 13:01:58 +0000

    implement field_map

--- src/field_map.rs
+++ src/field_map.rs
@@ -8,9 +8,12 @@ struct Field {
 
 impl Field {
 
-  fn field_tag(&self) -> Tag{
+    fn field_tag(&self) -> Tag{
+        self.field[0].tag
+    }
 
-      self.field[0].tag
-  }
-
+    fn init_field(&mut self, tag:Tag, value:&[u8])
+    {
+        self.field[0].init(tag, value);
+    }
 }
--- src/tag_value.rs
+++ src/tag_value.rs
@@ -2,75 +2,73 @@ use tag::*;
 use std::*;
 
 pub struct TagValue {
-  
-  pub tag: Tag,
-  pub value: Vec<u8>,
-  bytes: Vec<u8>
+    pub tag: Tag,
+    pub value: Vec<u8>,
+    bytes: Vec<u8>
 }
 
 impl TagValue {
 
-  fn new(tag_val: Tag, value: &[u8]) -> TagValue  {
-      
+    pub fn new(tag_val: Tag, value: &[u8]) -> TagValue {
+        let mut bytes: Vec<u8> = vec![];
+        bytes.extend(tag_val.to_num().to_string().as_bytes().iter().cloned());
+        bytes.extend("=".as_bytes().iter().cloned());
+        bytes.extend(value.iter().cloned());
+        bytes.extend("".as_bytes().iter().cloned());
 
-    let mut bytes: Vec<u8> = vec![];
-    
-    bytes.extend(tag_val.to_num().to_string().as_bytes().iter().cloned());
-    bytes.extend("=".as_bytes().iter().cloned());
-    bytes.extend(value.iter().cloned());
-
-    let mut  value_vec: Vec<u8> = vec![];
-    value_vec.extend(value);
-
-
-
-    TagValue{tag: tag_val, value: value_vec, bytes: bytes}
-
-  }
-
-
-  fn parse(raw_bytes: &[u8]) -> Result<TagValue, String> {
-
-    let sep_index = match raw_bytes.iter().position(|&c| c == b'=') {
-        Some(index) => index,
-        None => usize::max_value()
-    };
-
-    if sep_index == usize::max_value() {
-
-      return Err("TagValue.parse: '=' not found".to_string())
+        let mut  value_vec: Vec<u8> = vec![];
+        value_vec.extend(value);
+        TagValue{tag: tag_val, value: value_vec, bytes: bytes}
     }
 
+    pub fn init(&mut self, tag_val: Tag, value: &[u8]) {
+        let mut bytes: Vec<u8> = vec![];
+        bytes.extend(tag_val.to_num().to_string().as_bytes().iter().cloned());
+        bytes.extend("=".as_bytes().iter().cloned());
+        bytes.extend(value.iter().cloned());
+        bytes.extend("".as_bytes().iter().cloned());
 
-    let tag_string = str::from_utf8(raw_bytes).unwrap();
-    let tag_numstring = &tag_string[0..sep_index];
-    let tag_num = tag_numstring.parse::<i32>().unwrap();
-    let value_bytes = (&tag_string[(sep_index + 1)..]).as_bytes();
+        let mut  value_vec: Vec<u8> = vec![];
+        value_vec.extend(value);
 
-   
-    let tag_val = match Tag::from_number(tag_num) {
+        self.value = value_vec;
+        self.bytes = bytes;
+        self.tag = tag_val;
+    }
 
-        Some(tag) => tag,
-        None => return Err(format!("Unknown tag number {}", tag_num))
-    };
+    pub fn parse(raw_bytes: &[u8]) -> Result<TagValue, String> {
+        let sep_index = match raw_bytes.iter().position(|&c| c == b'=') {
+            Some(index) => index,
+            None => usize::max_value()
+        };
 
-    let mut bytes: Vec<u8> = vec![];
-    bytes.extend(raw_bytes);
+        if sep_index == usize::max_value() {
+          return Err("TagValue.parse: '=' not found".to_string())
+        }
 
-    let mut value_vec: Vec<u8> = vec![];
-    value_vec.extend(value_bytes);
+        let tag_string = str::from_utf8(raw_bytes).unwrap();
+        let tag_numstring = &tag_string[0..sep_index];
+        let tag_num = tag_numstring.parse::<i32>().unwrap();
+        let value_bytes = (&tag_string[(sep_index + 1)..]).as_bytes();
+        let tag_val = match Tag::from_number(tag_num) {
 
+            Some(tag) => tag,
+            None => return Err(format!("Unknown tag number {}", tag_num))
+        };
 
-    Ok(TagValue{tag: tag_val, value: value_vec, bytes: bytes})
+        let mut bytes: Vec<u8> = vec![];
+        bytes.extend(raw_bytes);
 
-  }
+        let mut value_vec: Vec<u8> = vec![];
+        value_vec.extend(value_bytes);
 
-  pub fn len(self:TagValue) -> usize  {
+        Ok(TagValue{tag: tag_val, value: value_vec, bytes: bytes})
+    }
 
-      self.bytes.len()
-  }
+    pub fn len(self:TagValue) -> usize {
+        self.bytes.len()
+    }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -78,26 +76,35 @@ mod test {
   use super::*; 
   use tag::*;
 
+    #[test]
+    fn new_test() {
+        let expected_value= "blahblah".as_bytes();
+        let expected_data= "8=blahblah".as_bytes();
+        let tag_value = TagValue::new(Tag::BeginString, expected_value);
+        assert_eq!(expected_value, tag_value.value.as_slice());
+        assert_eq!(expected_data, tag_value.bytes.as_slice());
+    }
 
-  #[test]
-  fn new_test()  {
-    let value = "A".as_bytes();
-    let tag_value = TagValue::new(Tag::HopRefID, value);
-    assert!(tag_value.len() == 5)
-  }
+    #[test]
+    fn init_test() {
+        let mut tag_value = TagValue{tag:Tag::BeginString, value:vec![], bytes:vec![]};
+        let expected_value= "blahblah".as_bytes();
+        let expected_data= "8=blahblah".as_bytes();
+        tag_value.init(Tag::BeginString, expected_value);
+        assert_eq!(expected_value, tag_value.value.as_slice());
+        assert_eq!(expected_data, tag_value.bytes.as_slice());
+    }
 
-  #[test]
-  fn parse_test()  {
-    let value = "35=A".as_bytes();
-    let result = TagValue::parse(value);
-    let res = match result {
-       Err(ref er) => false,
-      _ =>  true
-    };
-
-    assert!(res == true);
-    assert!("A" == String::from_utf8(result.unwrap().value).unwrap());
-  }
-
+    #[test]
+    fn parse_test() {
+        let value = "35=A".as_bytes();
+        let result = TagValue::parse(value);
+        let res = match result {
+           Err(ref er) => false,
+          _ =>  true
+        };
+        assert!(res == true);
+        assert!("A" == String::from_utf8(result.unwrap().value).unwrap());
+    }
 
 }
