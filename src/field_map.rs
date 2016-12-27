62aea0bb83b0b6b3292d5665dfcb63f00e5f4730
plemanach <plemanach@hotmail.com>
2016-12-27 17:44:27 +0000

    implement field_map

--- src/field_map.rs
+++ src/field_map.rs
@@ -1,6 +1,7 @@
 use tag::*;
 use std::*;
 use tag_value::*;
+use std::collections::*;
 
 struct Field {
      field: Vec<TagValue>
@@ -16,4 +17,28 @@ impl Field {
     {
         self.field[0].init(tag, value);
     }
+}
+
+type TagOrder = fn(i:Tag, j:Tag) -> bool;
+
+struct TagSort {
+    tags: Vec<Tag>,
+    compare: TagOrder
+}
+
+impl TagSort {
+
+    fn len(&self) -> usize { self.tags.len() }
+
+    fn swap(&mut self, i:usize, j:usize) {
+        let tmp = self.tags[i];
+        self.tags[i] = self.tags[j];
+        self.tags[j] = self.tags[i];
+    }
+    //fn less(&self, i:isize, j:isize) -> bool { self.tags[i]. }
+}
+
+struct FieldMap  {
+    tag_lookup: HashMap<Tag, Field>,
+    tag_sort: TagSort
 }
